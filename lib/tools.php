915437c49198940379a6cef5fdf87d43cdc2f9fe
a <a@a.a>
2020-05-11 20:12:44 +0000

    add Tools::readableBytes

--- lib/tools.php
+++ lib/tools.php
@@ -12,6 +12,13 @@ class Tools
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
+    static function readableBytes($bytes) {
+        $i = floor(log($bytes) / log(1024));
+        $sizes = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    
+        return sprintf('%.02F', $bytes / pow(1024, $i)) * 1 . ' ' . $sizes[$i];
+    }
+    
     static function msg($msg = 'empty', $return = true, $back = false)
     {
     	$time = time();
